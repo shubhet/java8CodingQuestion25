f0b02f931f5bc1726307a0c564ca83c9bd0a1117
Shubham Gupta <shubhet@gmail.com>
2025-10-20 22:29:52 +0530

    code added

---
+++ src/stream201025/StudentMain.java
@@ -0,0 +1,26 @@
+package stream201025;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class StudentMain {
+
+    public static void main(String[] args) {
+       List<Student> students = new ArrayList<>();
+       students.add(new Student(1, "Alice", 85));
+       students.add(new Student(2, "Bob", 90));
+       students.add(new Student(3, "Charlie", 78));
+       students.add(new Student(4, "Diana", 92));
+       students.add(new Student(5, "Ethan", 88));
+
+      List<Student> result = students.stream().filter(i->i.getGrade() >60).collect(Collectors.toList());
+
+      result.forEach(System.out::println);
+
+      List<String> result1 = students.stream().filter(i->i.getGrade()>70).map(i->i.getStudentName()).collect(Collectors.toList());
+      result1.forEach(System.out::println);
+
+
+    }
+}
