5bc2bf5f27d6438cd3b5c97177688f812670ecfb
Shubham Gupta <shubhet@gmail.com>
2025-10-22 18:26:10 +0530

    stream code added

--- src/stream201025/StringMain.java
+++ src/stream201025/StringMain.java
@@ -4,9 +4,5 @@ import java.util.Arrays;
 
 public class StringMain {
     public static void main(String[] args) {
-        String s = "Hello Java";
-        String[] s1 = s.split("");
-        String result = Arrays.stream(s1).reduce("",(a,c)->c+a);
-        System.out.println(result);
     }
 }
---
+++ src/stream221025/Java8Operations.java
@@ -0,0 +1,45 @@
+package stream221025;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class Java8Operations {
+    public static void main(String[] args) {
+
+        sumofDigit(123);
+        countVowels("Hello World");
+        countEachCharacter("hello world");
+        coundWordsInList(List.of("apple", "banana", "apple", "orange", "banana", "apple"));
+    }
+
+    static void sumofDigit(int n) {
+        String s = String.valueOf(n);
+        int sum = s.chars().map(Character::getNumericValue).sum();
+        System.out.println("Sum of digits: " + sum);
+        System.out.println("Sum of digits without stream: " + sum);
+    }
+
+    static void countVowels(String s) {
+        long count = s.toLowerCase().chars().mapToObj(c -> (char) c).filter(c -> "aeiou".indexOf(c) != -1).distinct().count();
+        System.out.println("Number of vowels: " + count);
+    }
+
+    static void countEachCharacter(String s) {
+        s= s.replaceAll("\\s+", "");
+        IntStream chars = s.trim().toLowerCase().chars();
+        Map<Character, Long> countResult = chars.mapToObj(c -> (char) c).collect(Collectors.groupingBy(i -> i, Collectors.counting()));
+        countResult.forEach((i, j) -> System.out.println(i + "===" + j));
+    }
+
+    static void coundWordsInList(List<String> words){
+        words.stream().collect(Collectors.groupingBy(i->i, Collectors.counting()))
+                .forEach((i, j)->System.out.println(i+"==="+j));
+
+    }
+
+
+
+}
