b767eef3a917a9e8ada7a29fb4a59bddc3bbc14e
Shubham Gupta <shubhet@gmail.com>
2026-01-10 11:14:48 +0530

    map data sorting

---
+++ src/javaStringList2601/ListOperation.java
@@ -0,0 +1,32 @@
+package javaStringList2601;
+
+import java.util.Comparator;
+import java.util.List;
+
+public class ListOperation {
+
+    public static void main(String[] args) {
+        List<Integer> list = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        System.out.println("Even numbers in the list:");
+        list.stream().filter(n -> n % 2 == 0).forEach(System.out::println);
+
+        System.out.println("Sum of all numbers in the list:");
+        int sum = list.stream().mapToInt(Integer::intValue).sum();
+        System.out.println(sum);
+
+        System.out.println("Average of all numbers in the list:");
+        var average = list.stream().mapToInt(Integer::intValue).average();
+        System.out.println(average);
+
+
+        System.out.println("Maximum and Minimum numbers in the list:");
+        // var max = list.stream().mapToInt(Integer::intValue).max();
+        var max = list.stream().sorted(Comparator.reverseOrder()).findFirst().get();
+
+        var min = list.stream().sorted(Comparator.naturalOrder()).findFirst().get();
+        System.out.println("Max: " + max);
+        System.out.println("Min: " + min);
+
+
+    }
+}
---
+++ src/javaStringList2601/StringOperation.java
@@ -0,0 +1,48 @@
+package javaStringList2601;
+
+import java.util.*;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+public class StringOperation {
+
+    public static void main(String[] args){
+
+        List<String> stringList = new ArrayList<>();
+        stringList.add("apple");
+        stringList.add("banana");
+        stringList.add("cherry");
+        stringList.add("apple");
+
+        //find occurrences of words
+        Map<String, Long> collectResult = stringList.stream().collect(Collectors.groupingBy(i ->i, Collectors.counting()));
+        System.out.println("Occurrences of words:");
+        collectResult.entrySet().forEach(System.out::println);
+
+        //print value in decreasing order
+        System.out.println("Occurrences of words in decreasing order:");
+       collectResult.entrySet().stream().sorted(Map.Entry.<String, Long>comparingByValue().reversed()).forEach(System.out::println);
+
+       //print first repeating char in string
+
+         String input = "swisosi";
+        Map<Character, Long> data = input.chars().mapToObj(c->(char)c).collect(Collectors.groupingBy(i->i,Collectors.counting()));
+        var datafinal = data.entrySet().stream().filter(i->i.getValue()>1).findFirst().get();
+        System.out.println("First repeating char in string:");
+        System.out.println(datafinal.getKey());
+
+        //print first non-repeating char in string
+        var datafinalNonRepeating = data.entrySet().stream().filter(i->i.getValue()==1).findFirst().get();
+        System.out.println("First non-repeating char in string:");
+        System.out.println(datafinalNonRepeating.getKey());
+
+
+        //reverse a string
+        String s = "hello world";
+        var datareverse = s.chars().mapToObj(c->String.valueOf((char)c)).reduce("",(a,b)->b+a);
+        System.out.println("Reversed string:");
+        System.out.println(datareverse);
+
+    }
+
+}
